41e12ee635b971a7af57dd8dd76c77119ffd45c6
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 21:58:18 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
     }
   }
 
-  contact.each do |person, data|
+  contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute = :favorite_icecream_flavors
         attribute.delete_if("strawberry")
