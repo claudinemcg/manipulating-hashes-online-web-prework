06993c217dd82d453bf1010f10875e514c628b03
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 21:49:08 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-contacts[:favourite_icecream_flavors].shift
+contacts[:favourite_icecream_flavors]
 
   #remember to return your newly altered contacts hash!
   contacts
