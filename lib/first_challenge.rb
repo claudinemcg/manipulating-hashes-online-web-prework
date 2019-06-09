5623fc6e1723e912be8932fe5ca0256ecc43827b
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 21:51:47 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-contacts[Freddy[:favourite_icecream_flavors]= "cookie dough", "mint chip"
+contacts["Freddy Mercury"][:favourite_icecream_flavors]= "cookie dough", "mint chip"
 
   #remember to return your newly altered contacts hash!
   contacts
