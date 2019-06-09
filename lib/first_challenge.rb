38b3ad604086ee591b5b24ae83fe1fe169eb8c83
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 21:53:11 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,5 @@
 def first_challenge
-  contacts["Freddy Mercury"][:favourite_icecream_flavors]= "cookie dough", "mint chip"
+  
   contacts = {
     "Jon Snow" => {
       name: "Jon",
@@ -13,7 +13,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
+contacts["Freddy Mercury"][:favourite_icecream_flavors]= "cookie dough", "mint chip"
 
 
   #remember to return your newly altered contacts hash!
