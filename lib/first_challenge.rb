3326e8430f48e35419e4c99bee454178d662b181
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 21:50:40 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-contacts[:favourite_icecream_flavors]
+contacts[:favourite_icecream_flavors]= "cookie dough", "mint chip"
 
   #remember to return your newly altered contacts hash!
   contacts
